7aaf17b76a19b2eeabcd0bfcd3920a7df9ec8956
SAMKIT-CHOPDA <samkitchopda04@gmail.com>
2025-10-07 14:54:56 +0530

    Create App.java

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,7 @@
+package com.example;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello from Jenkins! The Java app is running successfully and hence marks the completion of Assignment 8");
+    }
+}
